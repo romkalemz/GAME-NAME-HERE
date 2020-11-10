562963f6594aca64037dba0cebc114738988149a
Roman Stolyarov <romkas2000@gmail.com>
2020-11-09 19:31:18 -0800

    Created client & server .java files with necessary imports

---
+++ Time Lapse/src/time_lapse/Client.java
@@ -0,0 +1,12 @@
+package time_lapse;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+
+}
---
+++ Time Lapse/src/time_lapse/Server.java
@@ -0,0 +1,14 @@
+package time_lapse;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.ClassNotFoundException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	
+	
+
+}
