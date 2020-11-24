10a4c72b9974c0b509e273aa66b899ad54107eba
Roman Stolyarov <romkas2000@gmail.com>
2020-11-23 18:34:45 -0800

    Client code has been modulated and easier to read

--- Time Lapse/src/client_side/Client.java
+++ Time Lapse/src/client_side/Client.java
@@ -11,17 +11,35 @@ public class Client {
 	
 	public void connect() throws IOException {
 		
-		boolean connected = false;	//whether the client connected to server or not
-		Socket socket = null;
 		
 		// connect to server
 		// loop until the client connects to the server
+		Socket socket = attempt_to_connect();
+		
+		// connection successful!
+		// thread to read KB input and send to server
+		readKeyboard(socket);
+		
+		// read input from server, and render game
+		read_and_render(socket);
+		
+	}
+	
+	private void read_and_render(Socket server) {
+		
+	}
+	
+	private Socket attempt_to_connect() throws IOException {
+		boolean connected = false;	//whether the client connected to server or not
+		Socket socket = null;
+		
 		while(!connected) {
 			try {
 				InetAddress ip = InetAddress.getByName("localhost");
 				socket = new Socket(ip, 5056);
 				connected = true;
-				System.out.println("Connection Successful: " + socket);
+				System.out.println("Connection Successful: " + socket);				
+				break;
 				
 			} catch (ConnectException e) {
 				System.out.println("Failed to connect to server. Attempting in 3 seconds...");
@@ -31,31 +49,38 @@ public class Client {
 					ie.printStackTrace();
 				}
 			}
-		}
-		// connection successful, now read keyboard input and render game
-		try {
-			//TODO: STILL IN PROGRESS, ONLY READS TERMINAL INPUT 
-			Scanner clientIn = new Scanner(System.in);
-			// Set output stream
-			ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-        	
-			while(true) {
-				//read system.in
-				String sendMessage = clientIn.nextLine();
-				oos.writeObject(sendMessage);
-				if(sendMessage.equalsIgnoreCase("exit")) {
-					System.out.println("Client: client is closing");
-					break;
+		}	
+		return socket;
+	}
+	
+	private void readKeyboard(Socket socket) {
+		Thread readKB = new Thread() {
+			public void run() {
+				try {
+					Scanner clientIn = new Scanner(System.in);
+					// Set output stream
+					ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+		        	
+					while(true) {
+						//read system.in
+						String sendMessage = clientIn.nextLine();
+						oos.writeObject(sendMessage);
+						if(sendMessage.equalsIgnoreCase("exit")) {
+							System.out.println("Client: client is closing");
+							break;
+						}
+					}
+		        	
+		        	oos.close();
+		        	socket.close();
+		        	clientIn.close();
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
 			}
-        	
-        	oos.close();
-        	socket.close();
-        	clientIn.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
+		};
+		readKB.start();
 	}
+
 
 }
